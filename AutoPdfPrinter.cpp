7d49281ccd3c4ae27e269c523c97f641ca8ba2fb
PortreeKid <keith.paterson@gmx.de>
2022-01-25 20:39:02 +0100

    Version 0.2 Added modifiers for (:\) and wait for File save

--- AutoPdfPrinter.cpp
+++ AutoPdfPrinter.cpp
@@ -24,7 +24,9 @@ SOFTWARE.
 
 #define WIN32_LEAN_AND_MEAN
 #include <iostream>
+#include <vector>
 #include <windows.h>
+
 
 void sendEnter() {
     std::cout << "Sending Enter" << std::endl;
@@ -46,11 +48,29 @@ void sendEnter() {
     }
 }
 
+/*
+Creates an INPUT structure for Scancode (Unicode)
+*/
+
+INPUT createScanCodeEvent(WORD scancode, bool isDown)
+{
+    INPUT input = {};
+    input.type = INPUT_KEYBOARD;
+    input.ki.wVk = 0;
+    input.ki.wScan = scancode;
+    input.ki.dwFlags = (isDown ? 0 : KEYEVENTF_KEYUP) | KEYEVENTF_SCANCODE;
+    input.ki.time = 0;
+    input.ki.dwExtraInfo = 0;
+    return input;
+}
+
 int main(int argc, char* argv[])
 {
-    if (argc != 3) {
-        std::cout << "Usage : AutoPdfPrinter [secs to wait] [filename]" << std::endl;
-        std::cout << "[filename] can be an absolute path. " << std::endl;
+    if (argc != 4) {
+        std::cout << "Usage : AutoPdfPrinter secs_to_wait save_delay filename" << std::endl;
+        std::cout << "secs_to_wait Time to wait for the appearance of the print dialog" << std::endl;
+        std::cout << "save_delay Time to wait for the appearance of the save as dialog" << std::endl;
+        std::cout << " filename the filenameand path to be used" << std::endl;
         return 0;
     }
     wchar_t printDlgTitle[1024];
@@ -63,10 +83,11 @@ int main(int argc, char* argv[])
     LoadString(h, 505, saveAsDlgTitle, ARRAYSIZE(saveAsDlgTitle));
     FreeLibrary(h);
 
-    std::wcout << "AutoPdfPrinter 0.1 Searching : "  << printDlgTitle << " and " << saveAsDlgTitle << std::endl;
+    std::wcout << "AutoPdfPrinter 0.2 Searching : "  << printDlgTitle << " and " << saveAsDlgTitle << std::endl;
+
+    HWND hwnd = nullptr;
 
     int wait = atoi(argv[1]);
-    HWND hwnd = nullptr;
     while (--wait > 0 && !(hwnd = FindWindowW(0, printDlgTitle))) {
         Sleep(1000);
     }
@@ -80,9 +101,13 @@ int main(int argc, char* argv[])
     SetActiveWindow(hwnd);
     SetForegroundWindow(hwnd);
     sendEnter();
-    Sleep(1000);
-
     HWND saveHwnd = nullptr;
+
+    wait = atoi(argv[2]);
+    while (--wait > 0 && !(saveHwnd = FindWindowW(0, saveAsDlgTitle))) {
+        Sleep(1000);
+    }
+
     saveHwnd = FindWindowW(0, saveAsDlgTitle);
     if (!saveHwnd) {
         std::wcout << "File Save Dialog not found : " << saveHwnd << std::endl;
@@ -90,22 +115,54 @@ int main(int argc, char* argv[])
     }
     SetActiveWindow(saveHwnd);
     SetForegroundWindow(saveHwnd);
-    std::cout << "Sending " << argv[2] << " to " << saveHwnd << strlen(argv[2]) << std::endl;
+    std::cout << "Sending " << argv[3] << " to " << saveHwnd << strlen(argv[3]) << std::endl;
 
     HKL kbl = GetKeyboardLayout(0);
-    for (int i = 0; i < strlen(argv[2]);i++) {
+
+
+    for (int i = 0; i < strlen(argv[3]);i++) {
+        int keyCode = VkKeyScanExA(argv[3][i], kbl) & 0xff;
+
+        int modifier = (VkKeyScanExA(argv[3][i], kbl) & 0xff00)>>8;
+
+        if (modifier) {
+            std::vector<INPUT> keystroke;
+            if (modifier & 1) {
+                keystroke.push_back(createScanCodeEvent(0x2a, true));
+            }
+            if (modifier & 2) {
+                keystroke.push_back(createScanCodeEvent(0x1d, true));
+            }
+            if (modifier & 4) {
+                keystroke.push_back(createScanCodeEvent(0x38, true));
+            }
+            SendInput(keystroke.size(), keystroke.data(), sizeof(INPUT));
+        }
+
         INPUT saveinputs[2];
         ZeroMemory(saveinputs, sizeof(saveinputs));
         saveinputs[0].type = INPUT_KEYBOARD;
-        saveinputs[0].ki.wVk = VkKeyScanExA(argv[2][i], kbl);
+        saveinputs[0].ki.wVk = keyCode;
         saveinputs[1].type = INPUT_KEYBOARD;
-        saveinputs[1].ki.wVk = VkKeyScanExA(argv[2][i], kbl);
+        saveinputs[1].ki.wVk = keyCode;
         saveinputs[1].ki.dwFlags = KEYEVENTF_KEYUP;
-        std::cout << "Sending " << VkKeyScanExA(argv[2][i], kbl) <<  std::endl;
         UINT uSent = SendInput(ARRAYSIZE(saveinputs), saveinputs, sizeof(INPUT));
         if (uSent != 2)
         {
             std::cerr << "SendInput failed: 0x%x\n" << HRESULT_FROM_WIN32(GetLastError()) << std::endl;
+        }
+        if (modifier) {
+            std::vector<INPUT> keystroke;
+            if (modifier & 4) {
+                keystroke.push_back(createScanCodeEvent(0x38, false));
+            }
+            if (modifier & 2) {
+                keystroke.push_back(createScanCodeEvent(0x1d, false));
+            }
+            if (modifier & 1) {
+                keystroke.push_back(createScanCodeEvent(0x2a, false));
+            }
+            SendInput(keystroke.size(), keystroke.data(), sizeof(INPUT));
         }
     }
 
--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 A small program to automate printing. It will wait for the appearance of the print dialog, presses Ok, waits for the "Save as" 
 dialog, enters the file name and ends.
 
-Usage : AutoPdfPrinter [secs to wait] [filename]
+Usage : AutoPdfPrinter secs_to_wait save_delay filename
+
+* secs_to_wait Time to wait for the appearance of the print dialog
+* save_delay Time to wait for the appearance of the save as dialog
+* filename the filename and path to be used
